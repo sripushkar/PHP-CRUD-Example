57327eb8ac1c44a0e12220e37828d983eb23d6dc
Sri Julapally <sripushkar@gmail.com>
2020-07-30 13:47:26 -0400

    added delete functionality

--- process.php
+++ process.php
@@ -1,12 +1,20 @@
 <?php
 
+//DB connection
 $mysqli = new mysqli('localhost', 'root', 'root', 'crud') or die(mysqli_error($mysqli));
 
+//This executes when the save button is posted from index.php
 if(isset($_POST['save'])){
   $name = $_POST['name'];
   $location = $_POST['location'];
 
   $mysqli->query("INSERT INTO data(name, location) VALUES('$name', '$location')") or die($mysqli->error);
+}
+
+//This executes when the delete button is clicked and is passed into the url from index.php
+if(isset($_GET['delete'])){
+  $id = $_GET['delete'];
+  $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
 }
 
 ?>
