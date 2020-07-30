542fdcf219f049ed325a4def01ab3d6ca8306037
Sri Julapally <sripushkar@gmail.com>
2020-07-30 13:00:26 -0400

    Change name of root folder

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <title>PHP CRUD</title>
-  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+
   <script src="scripts.js"> </script>
   <!-- CSS only -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -14,16 +14,22 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </head>
   <body>
+    <?php require_once 'process.php'; ?>
+
+    <div class="row justify-content-center">
     <form action="index.php" method="post">
       <div class="form-group">
-      <label>Name</label>
-      <input type="text" name="name" value="Enter your name">
-
-      <label>Location</label>
-      <input type="text" name="location" value="Enter your location">
-      <button type="submit" name="save">Save</button>
+        <label>Name</label>
+        <input type="text" name="name" class="form-control" value="Enter your name">
+      </div>
+      <div class="form-group">
+        <label>Location</label>
+        <input type="text" name="location" class="form-control" value="Enter your location">
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary" name="save">Save</button>
       </div>
     </form>
-
+    </div>
   </body>
 </html>
---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+
+$mysqli = new mysqli('localhost', 'root', 'root', 'crud') or die(mysqli_error($mysqli));
+
+if(isset($_POST['save'])){
+  $name = $_POST['name'];
+  $location = $_POST['location'];
+
+  $mysqli->query("INSERT INTO data(name, location) VALUES('$name', '$location')") or die($mysqli->error);
+}
+
+?>
