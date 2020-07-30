b650dccef57ec364551e24b178935d3e5bb19ed5
Sri Julapally <sripushkar@gmail.com>
2020-07-30 13:52:49 -0400

    added session vars

--- process.php
+++ process.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 //DB connection
 $mysqli = new mysqli('localhost', 'root', 'root', 'crud') or die(mysqli_error($mysqli));
 
@@ -9,12 +11,22 @@ if(isset($_POST['save'])){
   $location = $_POST['location'];
 
   $mysqli->query("INSERT INTO data(name, location) VALUES('$name', '$location')") or die($mysqli->error);
+
+  $_SESSION['message'] = "Record has been saved!";
+  $_SESSION['msg_type'] = "success";
+
+  header('location: index.php');
 }
 
 //This executes when the delete button is clicked and is passed into the url from index.php
 if(isset($_GET['delete'])){
   $id = $_GET['delete'];
   $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
+
+  $_SESSION['message'] = "Record has been deleted!";
+  $_SESSION['msg_type'] = "danger";
+
+  header('location: index.php');
 }
 
 ?>
